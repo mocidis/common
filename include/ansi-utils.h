a468adf052f48093cf02f76f24ef9f5d0e4372ff
Tung HOANG <tung.hoang@gmail.com>
2015-08-26 21:35:08 +0700

    add ansi utils functions

--- include/ansi-utils.h
+++ include/ansi-utils.h
@@ -1,4 +1,21 @@
 #ifndef __ANSI_UTILS_H__
 #define __ANSI_UTILS_H__
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 void my_pause();
+#define CHECK(f, op) \
+do { \
+			if ( 0 != op ) { \
+				fprintf(stderr, "%s - %s error: %s\n", f, #op, strerror(errno)); \
+				exit(-1); \
+			} \
+}  while(0)
+
+#define PANIC(f, msg) \
+    fprintf(stderr, "%s - %s\n", f, msg);\
+    exit(-1)
+
 #endif
