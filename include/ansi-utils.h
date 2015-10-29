55c1233ef11c610500c20af526d017634e3705db
buidanhquy <quy.buidanhquy@gmail.com>
2015-10-29 16:58:53 +0700

    update ansi utils

--- include/ansi-utils.h
+++ include/ansi-utils.h
@@ -7,7 +7,12 @@
 #include <unistd.h>
 void my_pause();
 
+void ansi_copy_str(char *dest, char *src);
+
 extern int THRESHOLD;
+
+#define UNUSED(x) \
+    (void)(x)
 
 #define SET_LOG_LEVEL(level) \
     if (level >= 1 && level <=5) THRESHOLD = level
@@ -31,9 +36,16 @@ extern int THRESHOLD;
     fprintf(stderr, message); \
     exit(-1); \
 }
+
+#define PERROR_IF_TRUE(clause, message) if(clause) { \
+    SHOW_LOG(3, fprintf(stdout, "ERROR:: %s - %d - %s\n", message, errno, strerror(errno))); \
+}
+
 #define RETURN_IF_TRUE(clause, message) if(clause) { \
     fprintf(stderr, message); \
     exit(-1); \
 }
+
+void extract_ip(char *source, char *des);
 
 #endif
--- src/ansi-utils.c
+++ src/ansi-utils.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int THRESHOLD = 3;
 
@@ -7,4 +8,19 @@ void my_pause() {
     if(fgets(temp, sizeof(temp), stdin) == NULL) {
         printf("Error Null pointer return.\n");
     }
+}
+void ansi_copy_str(char *dest, char *source) {
+    int len = strlen(source);
+    strncpy(dest, source, len);
+    dest[len] = '\0';
+}
+
+void extract_ip(char *source, char *des) {
+    char *temp, *temp2;
+    temp = strchr(source, ':');
+    temp++;
+    temp2 = strchr(temp, ':');
+   // memset(des, 0, sizeof(des));
+    strncpy(des, temp, temp2 -temp);
+    des[temp2-temp] = '\0';
 }
