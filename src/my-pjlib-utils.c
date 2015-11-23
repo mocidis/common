72d30eb2f98893796c922c8a09127da196dc5d17
Tung HOANG <tung.hoang@gmail.com>
2015-11-23 10:02:27 +0700

    fix for portability

--- src/my-pjlib-utils.c
+++ src/my-pjlib-utils.c
@@ -1,10 +1,15 @@
 #include "my-pjlib-utils.h"
 
+void exit_default(int retcode) { exit(retcode); }
+
 pj_status_t __mpu_status;
+void (*__exit_f)(int retcode) = &exit_default;
+
 void err(const char *__file, const char *op, pj_status_t status) {
 	char errmsg[PJ_ERR_MSG_SIZE];
 	pj_strerror(status, errmsg, sizeof(errmsg));
-	PJ_LOG(3, (__file, "%s error: %s", op, errmsg));
+    fprintf(stderr, "\n%s: %s error: %s\n", __file, op, errmsg);
+	//PJ_LOG(3, (__file, "%s error: %s", op, errmsg));
 }
 
 void setup_addr_with_port(pj_sockaddr_in *paddr, int port) {
